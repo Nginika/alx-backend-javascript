eb8eec4b377f0c24092d73e79bed8bbf926a5897
Nginika <nginikaugochukwu@gmail.com>
2023-06-26 18:17:28 +0000

    check elint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    // disable line
     const task = true;
+    // disable line
     const task2 = false;
   }
 
