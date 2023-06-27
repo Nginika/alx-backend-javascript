21b6d2fc5997893801c47e739babb3f37f29a095
Nginika <nginikaugochukwu@gmail.com>
2023-06-27 19:11:38 +0000

    sue then catch and finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,7 @@
-export default function handleResponseFromAPI(promise) {
-  promise
-    .then(() => {
-      console.log('Got a response from the API');
-      const result = {
-        status: 200,
-        body: 'Success',
-      };
-      return result;
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      const emptyError = new Error();
-      return emptyError;
-    });
+export default function handledResponseFromAPI(promise) {
+  const res = { status: 200, body: 'success' };
+  return promise
+    .then(() => res)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
