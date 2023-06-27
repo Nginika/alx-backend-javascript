1dd16a9a5ff450cc4f42b3f4d62294377036d638
Nginika <nginikaugochukwu@gmail.com>
2023-06-27 20:12:34 +0000

    modify promise all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastname}`);
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
